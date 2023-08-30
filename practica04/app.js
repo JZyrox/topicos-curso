9130eaab544bb93a11128fddecf1a18d60631730
Daniela Hernandez <xdannix2626@gmail.com>
2023-08-30 14:16:21 -0700

    ya jala el codigo :fire:

--- practica04/app.js
+++ practica04/app.js
@@ -1,3 +1,5 @@
+const estudiantes = ['Daniela', 'Julio','Chris','Kevin'];
+
 const titulo = document.createElement('h1');
 titulo.innerHTML = 'FUNCIONES';
 
@@ -17,6 +19,11 @@ titulo.addEventListener('click', () => {
 
 borrar.addEventListener('click', () => {
   const p = document.querySelectorAll('p');
-  console.log(p);
+  for(let i=0; i < p.length; i++){
+    document.body.removeChild(p[i]);
+  }
 });
 
+for (let i=0; i < estudiantes.length; i++) {
+  console.log(estudiantes[i])
+}
