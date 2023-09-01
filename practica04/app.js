dd21334455207d544eb9d584d52a3a23150f17e0
Daniela Hernandez <xdannix2626@gmail.com>
2023-09-01 09:51:09 -0700

    ya jala el codigo :fire:

--- practica04/app.js
+++ practica04/app.js
@@ -17,12 +17,19 @@ titulo.addEventListener('click', () => {
   }
 });
 
+// Agregamos un evento 'click' al elemento con el id 'borrar'
 borrar.addEventListener('click', () => {
+  // Seleccionamos todos los elementos 'p' en el documento y los almacenamos en la variable 'p'
   const p = document.querySelectorAll('p');
-  for(let i=0; i < p.length; i++){
+  
+  // Iniciamos un bucle 'for' que itera desde 0 hasta la longitud de 'p'
+  for(let i=0; i > p.length; i++){
+    // Eliminamos el elemento 'p[i]' del documento
     document.body.removeChild(p[i]);
   }
 });
+
+
 
 for (let i=0; i < estudiantes.length; i++) {
   console.log(estudiantes[i])
